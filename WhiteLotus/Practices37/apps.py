a693c96c2463d0144529e016e25a4bc122c29275
Roman Kozulia <rk8318@rit.edu>
2019-10-17 19:21:35 -0400

    Added requirements file

---
+++ WhiteLotus/Practices37/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Practices37Config(AppConfig):
+    name = 'Practices37'
---
+++ WhiteLotus/dailyprayer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DailyprayerConfig(AppConfig):
+    name = 'dailyprayer'
