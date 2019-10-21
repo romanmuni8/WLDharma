fa6dbd8f2359aa19a2e8e2fbc1e01afbba6f98be
Patrick Wilbur <pjwilbur99@gmail.com>
2019-10-21 15:38:50 -0400

    register event model for admin app

--- WhiteLotus/calendar/admin.py
+++ WhiteLotus/calendar/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Event
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'notes']
