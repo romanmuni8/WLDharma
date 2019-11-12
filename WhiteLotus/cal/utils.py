bdc5e87a845589e25e5c063e93e135c0e3c24063
Patrick Wilbur <pjwilbur99@gmail.com>
2019-11-12 02:18:57 -0500

    more calendar fixes

--- WhiteLotus/cal/utils.py
+++ WhiteLotus/cal/utils.py
@@ -17,24 +17,24 @@ class Calendar(HTMLCalendar):
 		events_per_day = events.filter(start_time__day=day)
 		d = ''
 		for event in events_per_day:
-			d += f'<li> {event.title} </li>'
+			d += f'<dt> {event.title} </dt><dd> {event.description} </dd>'
 
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
 		return '<td></td>'
 
-	def formatweek(self, theweek):
+	def formatweek(self, theweek, events):
 		week = ''
 		for d, weekday in theweek:
-			week += self.formatday(d, self.events)
+			week += self.formatday(d, events)
 		return f'<tr> {week} </tr>'
 
 	def formatmonth(self, year, month, withyear=True):
-		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
+		events = Event.objects.filter(start_time__year= year, start_time__month= month)
 
 		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
-		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+		cal += f'{self.formatmonthname(year, month, withyear=withyear)}\n'
 		cal += f'{self.formatweekheader()}\n'
-		for week in self.monthdays2calendar(self.year, self.month):
+		for week in self.monthdays2calendar(year, month):
 			cal += f'{self.formatweek(week, events)}\n'
 		return cal
