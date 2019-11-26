6eebb971467325a1c89b7093d294bdda6dbf62b4
Patrick Wilbur <pjwilbur99@gmail.com>
2019-11-25 23:07:49 -0500

    started news model

--- WhiteLotus/currentEvent/models.py
+++ WhiteLotus/currentEvent/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.CharField(max_length=400)
+
+
+class Images(models.Model):
+    post = models.ForeignKey(News, on_delete=models.CASCADE())
+    image = models.ImageField()
