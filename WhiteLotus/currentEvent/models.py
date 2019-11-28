fa89ae584be1499ef5fbecd71205a86bac38801c
pjw9963 <pjw9963@rit.edu>
2019-11-27 19:05:05 -0500

    added news model

--- WhiteLotus/currentEvent/models.py
+++ WhiteLotus/currentEvent/models.py
@@ -7,7 +7,14 @@ class News(models.Model):
     title = models.CharField(max_length=128)
     body = models.CharField(max_length=400)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News"
+
 
 class Images(models.Model):
-    post = models.ForeignKey(News, on_delete=models.CASCADE())
+    post = models.ForeignKey(News, on_delete=models.CASCADE)
     image = models.ImageField()
+
