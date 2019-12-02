6f399ca4b87be09cffccc127cabfc0ebe88b221a
pjw9963 <pjw9963@rit.edu>
2019-12-02 15:24:56 -0500

    removed useless code and changed dailyprayer url to render correct template

--- WhiteLotus/WhiteLotusWeb/views.py
+++ WhiteLotus/WhiteLotusWeb/views.py
@@ -6,18 +6,6 @@ def home(request):
     return render(request, 'home.html')
 
 
-def about(request):
-    return render(request, "about.html")
-
-
-def calendar(request):
-    return render(request, "cal.calendar.html")
-
-
-def events(request):
-    return render(request,"events.html")
-
-
 def pictures(request):
     return render("pictures.html")
 
@@ -25,6 +13,3 @@ def pictures(request):
 def membership(request):
     return render("membership.html")
 
-
-def dailyprayer(request):
-    return render(request, "dailyPrayer.html")
