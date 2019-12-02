be905fd0da0ca3ca6b4debf7a2ae1760b32ebc76
Patrick Wilbur <pjwilbur99@gmail.com>
2019-12-01 23:09:20 -0500

    completed front end for news

--- WhiteLotus/currentEvent/models.py
+++ WhiteLotus/currentEvent/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=128)
-    body = models.CharField(max_length=400)
+    body = models.TextField()
 
     def __str__(self):
         return self.title
--- WhiteLotus/currentEvent/views.py
+++ WhiteLotus/currentEvent/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import News, Images
 from django.apps import apps
 Event = apps.get_model('cal', 'Event')
 
 
 def events(request):
     event_list = Event.objects.all()
-    return render(request, 'currentEvent/events.html', {'event_list': event_list})
+    news_list = News.objects.all()
+    images = Images.objects.all()
+    return render(request, 'currentEvent/events.html', {'event_list': event_list, 'news_list': news_list, 'news_images': images})
