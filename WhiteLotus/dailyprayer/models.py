0dadbd63cce619f6194e27e4b5dc13439c4d108b
pjw9963 <pjw9963@rit.edu>
2019-12-02 16:12:56 -0500

    created view and added backend logic for daily prayer model.

--- WhiteLotus/dailyprayer/models.py
+++ WhiteLotus/dailyprayer/models.py
@@ -1,11 +1,25 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Practice(models.Model):
     number = models.IntegerField()
-    text = models.CharField(max_length=None)
+    text = models.TextField()
+
+
+class DailyPracticeManager(models.Manager):
+    def create_daily_practice(self, chosen, choose_date):
+        dp = self.create(chosen=chosen, choose_date=choose_date)
+        return dp
 
 
 class DailyPractice(models.Model):
-    chosen = models.ForeignKey(Practice, on_delete=models.SET_NULL(), null=True)
+    chosen = models.ForeignKey(Practice, on_delete=models.SET_NULL, null=True)
     choose_date = models.DateField()
+
+    objects = DailyPracticeManager()
+
+    def save(self, *args, **kwargs):
+        if not self.pk and DailyPractice.objects.exists():
+            raise ValidationError('There is can be only one Daily Practice instance')
+        return super(DailyPractice, self).save(*args, **kwargs)
--- WhiteLotus/dailyprayer/views.py
+++ WhiteLotus/dailyprayer/views.py
@@ -1,5 +1,28 @@
+from random import randrange
+
+from django.db.models import Max
 from django.shortcuts import render
+from .models import DailyPractice, Practice, DailyPracticeManager
+from datetime import date
 
 
 def daily_prayer(request):
+    dp = None
+    if DailyPractice.objects.count() != 0:
+        dp = DailyPractice.objects.get()
+        if dp.choose_date != date.today():
+            dp.chosen = get_random_practice()
+            dp.choose_date = date.today()
+            dp.save()
+    else:
+        dp = DailyPractice.objects.create_daily_practice(chosen=get_random_practice(), choose_date=date.today())
     return render(request, 'dailyprayer/dailyprayer.html')
+
+
+def get_random_practice():
+    max_id = Practice.objects.all().aggregate(max_id=Max("id"))['max_id']
+    while True:
+        pk = randrange(1, max_id)
+        practice = Practice.objects.filter(pk=pk).first()
+        if practice:
+            return practice
