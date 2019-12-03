f3f448d6bb917e5090447da34b5e66ea26791102
pjw9963 <pjw9963@rit.edu>
2019-12-02 22:58:57 -0500

    homepage looks great now

--- WhiteLotus/WhiteLotusWeb/views.py
+++ WhiteLotus/WhiteLotusWeb/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.apps import apps
+from datetime import date, timedelta
+
+Event = apps.get_model('cal', 'Event')
+DailyPrayer = apps.get_model('dailyprayer', 'DailyPractice')
+About = apps.get_model('about', 'AboutPageText')
 
 
 def home(request):
-    return render(request, 'home.html')
+    upcoming = Event.objects.raw("SELECT * FROM cal_event WHERE start_time BETWEEN \'" + str(date.today()) + "\' AND \'" + str(date.today() + timedelta(30)) + "\'")
+    return render(request, 'home.html', {'about': About.objects.get().text,
+                                         'event_list': upcoming,
+                                         'daily': DailyPrayer.objects.get()})
 
 
 def pictures(request):
